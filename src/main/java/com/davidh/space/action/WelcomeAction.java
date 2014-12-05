fb3d34b45529793fd6872f56eb788f76e6bc85a3
unknown <david.hines@CANCQKGMX1.corp.ad.cginet>
2014-12-05 16:17:14 -0400

    I am getting an resource unavailable exception.

--- src/main/java/com/davidh/space/action/WelcomeAction.java
+++ src/main/java/com/davidh/space/action/WelcomeAction.java
@@ -7,6 +7,10 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.ContextCleanupListener;
+
 
 public class WelcomeAction extends Action{
     @Override
@@ -15,6 +19,7 @@ public class WelcomeAction extends Action{
             throws Exception {
 
         request.setAttribute("message", "Spring security with struts 1.3  success page");
+
         return mapping.findForward("success");
     }
 
